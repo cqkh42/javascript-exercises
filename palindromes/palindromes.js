7fdb30bfda48e2f3b94994f1cf9e8f5997e2216f
cqkh42 <jackcooper93@gmail.com>
2020-05-27 15:43:49 +0100

    finished palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+  const first = string.toLowerCase().replace(/[^a-z]/g, "").split("")
+  const second = [...first].reverse()
+  return first.join("") == second.join("")
 }
 
 module.exports = palindromes
