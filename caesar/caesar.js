bbe07f4917dbc7686259b38890b7a5a79f4dcc41
cqkh42 <jackcooper93@gmail.com>
2020-05-29 10:41:24 +0100

    caesar complete

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,14 @@
-const caesar = function() {
-
+const caesar = function(string, jumps) {
+  const arr = string.split("")
+  let charCodes = arr.map(char => char.charCodeAt(0))
+  const offsets = [65, 97]
+  for (start in offsets) {
+    let val = offsets[start]
+    console.log(offsets[start])
+    charCodes = charCodes.map(code => ((code >= val) && (code <= val+25)) ? (code+jumps-val+26)%26 + val : code)
+  }
+  const newChars = charCodes.map(code => String.fromCharCode(code))
+  return newChars.join("")
 }
 
 module.exports = caesar
