bfc4c80944dc494da49b11c6858a52fc34403a38
cqkh42 <jackcooper93@gmail.com>
2020-05-29 10:49:33 +0100

    fibonacci complete

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(numbers) {
+  let first = 1
+  let second = 1
+  if (numbers < 0) {
+    return "OOPS"
+  }
+  for (i = 0; i < numbers - 2; i++) {
+    let t = first + second
+    first = second
+    second = t
+  }
+  return second
 }
 
 module.exports = fibonacci
