43ecce4dd28c6238bba9a3975e03f73da16d1b08
cqkh42 <jackcooper93@gmail.com>
2020-05-29 14:39:40 +0100

    find the oldest complete

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(object) {
+  const year = new Date().getFullYear()
+  object.sort((a, b) => (a.yearOfDeath || year) - (a.yearOfBirth || year) > (b.yearOfDeath || year) - (b.yearOfBirth || year) ? -1 : 1)
+  return object[0]
 }
 
 module.exports = findTheOldest
